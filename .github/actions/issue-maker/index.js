0813cfe65085374ab9e87de64238a8f874945258
jmartinezl <jesus.martinez.lorenzo@hotmail.com>
2022-02-15 20:15:03 -0600

    create index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,17 +7,17 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = github.getOctokit(token);
+    const octokit = new github.getOctokit(token);
 
     const newIssue = await octokit.issues.create({
-        repo: github.context.repo.repo,
-        owner: github.context.repo.owner,
-        title: issueTitle,
-        body; jokeBody
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
+      title: issueTitle,
+      body: jokeBody
     });
-  } catch (err) {
-      core.setFailed(err.message);
+  } catch (error) {
+    core.setFailed(error.message);
   }
 }
 
-run()
+run();
